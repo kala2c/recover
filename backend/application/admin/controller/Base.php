f1b2ca97eefc2fb1f6e8c418de24f07bddb55608
lixiaole <lixiaole2015@gmail.com>
2020-04-16 08:53:04 +0800

    取货员账号状态

--- backend/application/admin/controller/Base.php
+++ backend/application/admin/controller/Base.php
@@ -8,6 +8,7 @@ use app\common\BaseController;
 use app\common\model\OrderMaster as OrderModel;
 use app\common\model\Waste as WasteModel;
 use app\common\model\User as UserModel;
+use app\common\model\Pickman as PickmanModel;
 
 class Base extends BaseController
 {
@@ -18,22 +19,27 @@ class Base extends BaseController
         $wastecount = WasteModel::count();
         //用户总数
         $allusercount = UserModel::count();
-        //今天新增用户数
-        //$todayusercount = UserModel::where('createtime','=',tody)->count();
+        //今日新增用户数
+        $todayusercount = UserModel::whereTime('create_time','today')->count();
 
         //配送员总数
-
+        $pickmancount = PickmanModel::where('status', '=', 1)->count();
         //总成交订单数
-//        $allordercount = OrderModel::where('status','=',OrderModel::STATUS_SUCCESS)->count();
+        $allordercount = OrderModel::where('status','=',OrderModel::STATUS_SUCCESS)->count();
         //今日成交订单数
-//        $todayordercount = OrderModel::where('status','=',OrderModel::STATUS_SUCCESS)->count();
+        $todayordercount = OrderModel::where('status','=',OrderModel::STATUS_SUCCESS)->whereTime('create_time','today')->count();
         //今日下单数
 //        $todayordercount = OrderModel::where('','=',OrderModel::STATUS_SUCCESS)->count();
         //今日成交额
+//        $todayordersum = OrderModel::where('status','=',OrderModel::STATUS_SUCCESS)->whereTime('create_time','today')->count()
         $result_map = [
             'dashboarddata' => [
                 'wastecount' => $wastecount,
-                'allusercount' => $allusercount
+                'allusercount' => $allusercount,
+                'pickmancount' => $pickmancount,
+                'todayusercount' => $todayusercount,
+                'allordercount'=>$allordercount,
+                'todayordercount'=>$todayordercount
             ]
         ];
         return success($result_map);
--- backend/application/admin/controller/Pickman.php
+++ backend/application/admin/controller/Pickman.php
@@ -7,6 +7,21 @@ use app\common\model\Pickman as PickmanModel;
 
 class Pickman extends Base
 {
+    const STATUS_INIT = 0;
+    const STATUS_NORMAL = 1;
+    const STATUS_SUBAUTH = 2;
+    const STATUS_FREEZE = 3;
+
+    const PICK_FAST = 1;
+    const PICK_SUB = 2;
+
+    static public $PICKMAN_STATUS = [
+        self::STATUS_INIT => '未提交认证',           //即刚新建的账号
+        self::STATUS_NORMAL => '正常',              //通过认证的
+        self::STATUS_SUBAUTH => '已提交认证',        //已提交认证信息但管理员还没有审核通过
+        self::STATUS_FREEZE => '冻结'
+    ];
+
     //返回取货员列表
     public function PickmanList()
     {
